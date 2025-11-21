c9b9fb91ef1c71bd3ca43e873814a125820653ec
fss1208 <fss1208@naver.com>
2025-11-21 17:26:59 +0900

    20251121c_SeSAC_pandas_chart

--- 20251120_matplotlib.py
+++ 20251120_matplotlib.py
@@ -2,6 +2,7 @@ import platform
 import matplotlib as mpl
 from matplotlib import font_manager, rc
 import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
 import sys, KSH
 
@@ -114,6 +115,7 @@ def test_matplotlib_pie(): # 비율
     plt.pie(result, labels=fruit, autopct="%.1f%%", startangle=90, counterclock=False, explode=explode_value, shadow=True)
     plt.show()
 
+@KSH.func_decorator
 def test_matplotlib_save():
     fruit = ['apple', 'banana', 'strawberry', 'orange', 'grape']
     result = [7, 6, 3, 2, 2]
@@ -123,6 +125,64 @@ def test_matplotlib_save():
     plt.savefig("pie_chart_100.png", dpi=100)
     plt.savefig("pie_chart_200.png", dpi=200)
     plt.savefig("pie_chart_200.jpg", dpi=200)
+
+@KSH.func_decorator
+def test_matplotlib_pandas_line():
+    test_matplotlib()
+    year = [2006, 2008, 2010, 2012, 2014, 2016] # 연도
+    area = [26.2, 27.8, 28.5, 31.7, 33.5, 33.2] # 1인당 주거면적
+    table = {'연도':year, '주거면적':area}
+    df_area = pd.DataFrame(table, columns=['연도', '주거면적'])
+    df_area.plot(x='연도', y='주거면적', grid=True, title='연도별 1인당 주거면적')
+    plt.show()
+
+@KSH.func_decorator
+def test_matplotlib_pandas_scatter():
+    test_matplotlib()
+    temperature = [25.2, 27.4, 22.9, 26.2, 29.5, 33.1, 30.4, 36.1, 34.4, 29.1]
+    Ice_cream_sales = [236500, 357500, 203500, 365200, 446600, 574200, 453200, 675400, 598400, 463100]
+    dict_data = {'기온':temperature, '아이스크림 판매량':Ice_cream_sales}
+    df_ice_cream = pd.DataFrame(dict_data, columns=['기온', '아이스크림 판매량'])
+    df_ice_cream.plot.scatter(x='기온', y='아이스크림 판매량', grid=True, title='최고')
+    #df_ice_cream.plot(kind="scatter", x='기온', y='아이스크림 판매량', grid=True, title='최고')
+    plt.show()
+
+@KSH.func_decorator
+def test_matplotlib_pandas_bar():
+    test_matplotlib()
+    grade_num = [5, 14, 12, 3]
+    students = ['A', 'B', 'C', 'D']
+    df_grade = pd.DataFrame(grade_num, index=students, columns = ['Student'])
+    grade_bar = df_grade.plot.bar(grid = True)
+    grade_bar.set_xlabel("학점")
+    grade_bar.set_ylabel("학생수")
+    grade_bar.set_title("학점별 학생 수 막대 그래프")
+    plt.show()
+
+@KSH.func_decorator
+def test_matplotlib_pandas_histogram():
+    test_matplotlib()
+    math = [76,82,84,83,90,86,85,92,72,71,100,87,81,76,
+    94,78,81,60,79,69,74,87,82,68,79]
+    df_math = pd.DataFrame(math, columns = ['Student'])
+    math_hist = df_math.plot.hist(bins=8, grid = True)
+    math_hist.set_xlabel("시험 점수")
+    math_hist.set_ylabel("도수(frequency)")
+    math_hist.set_title("수학 시험의 히스토그램")
+    plt.show()
+
+@KSH.func_decorator
+def test_matplotlib_pandas_pie():
+    test_matplotlib()
+    fruit = ['사과', '바나나', '딸기', '오렌지', '포도']
+    result = [7, 6, 3, 2, 2]
+    df_fruit = pd.Series(result, index = fruit, name = '선택한 학생수')
+    explode_value = (0.1, 0, 0, 0, 0)
+    fruit_pie = df_fruit.plot.pie(figsize=(5, 5), autopct='%.1f%%', startangle=90,
+    counterclock = False, explode=explode_value, shadow=True, table=True)
+    fruit_pie.set_ylabel("") # 불필요한 y축 라벨 제거
+    fruit_pie.set_title("과일 선호도 조사 결과")
+    plt.show()
 
 if (__name__ == "__main__"):
     try:
@@ -136,6 +196,11 @@ if (__name__ == "__main__"):
             print("5. test_matplotlib_histogram()")
             print("6. test_matplotlib_pie()")
             print("7. test_matplotlib_save()")
+            print("11. test_matplotlib_pandas_line()")
+            print("12. test_matplotlib_pandas_scatter()")
+            print("13. test_matplotlib_pandas_bar()")
+            print("14. test_matplotlib_pandas_histogram()")
+            print("15. test_matplotlib_pandas_pie()")
             menu = int(input("please, input menu number? "))
             if (menu == 1): test_matplotlib()
             elif (menu == 2): test_matplotlib_line()
@@ -144,6 +209,11 @@ if (__name__ == "__main__"):
             elif (menu == 5): test_matplotlib_histogram()
             elif (menu == 6): test_matplotlib_pie()
             elif (menu == 7): test_matplotlib_save()
+            elif (menu == 11): test_matplotlib_pandas_line()
+            elif (menu == 12): test_matplotlib_pandas_scatter()
+            elif (menu == 13): test_matplotlib_pandas_bar()
+            elif (menu == 14): test_matplotlib_pandas_histogram()
+            elif (menu == 15): test_matplotlib_pandas_pie()
             else: break
     except Exception as ex:
         print("# [NG] {} {}".format(type(ex), ex))
